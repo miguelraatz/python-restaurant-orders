f7f3c6ea4fa5ee8b556748da3f0d7f407e474d84
miguelraatz <miguelraatz_@hotmail.com>
2023-07-20 15:23:40 -0300

    tests: implemented tests for class ingredient - requirement 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,27 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # Instance
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("ovo")
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
+
+    # repr
+    assert repr(ingredient1) == "Ingredient('queijo mussarela')"
+    assert ingredient3.name == "ovo"
+
+    # equal
+    assert ingredient1 == ingredient2
+    assert ingredient2 != ingredient3
+
+    # restrictions
+    assert ingredient1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert ingredient3.restrictions == {Restriction.ANIMAL_DERIVED}
